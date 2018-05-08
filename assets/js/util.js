34ac5c66eb654f4305211f94a91b270dad48babd
-k <slowdive@me.com>
2018-05-08 09:16:27 -0400

    fix if condition

--- assets/js/util.js
+++ assets/js/util.js
@@ -42,9 +42,9 @@
 	$.fn.panel = function(userConfig) {
 
 		// No elements?
-			if (this.length === 0)
+			if (this.length === 0) {
 				return $this;
-
+				}
 		// Multiple elements?
 			if (this.length > 1) {
 
