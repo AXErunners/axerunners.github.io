f94a8981f55f13fbc7f6c6272bb5e1d775bd8b5f
-k <slowdive@me.com>
2018-05-08 15:03:54 -0400

    edit jquery syntax

--- assets/js/util.js
+++ assets/js/util.js
@@ -95,7 +95,7 @@
 			}, userConfig);
 
 			// Expand "target" if it's not a jQuery object already.
-				if (typeof config.target != 'jQuery') {
+				if (typeof config.target != "jQuery") {
 					config.target = $(config.target);
 				}
 
