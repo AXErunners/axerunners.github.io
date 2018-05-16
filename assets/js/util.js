f1a12b18b134dd43e9584bc1f3d377250b34889d
-k <slowdive@me.com>
2018-05-15 21:12:10 -0400

    add more eq

--- assets/js/util.js
+++ assets/js/util.js
@@ -482,7 +482,7 @@
 
 									x = i.parent().find('input[name=' + i.attr('name') + '-polyfill-field]');
 
-									if (i.val() == '') {
+									if (i.val() === '') {
 										i.hide();
 										x.show();
 									}
