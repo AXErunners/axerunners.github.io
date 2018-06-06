9fbc045497d45befea25299215411b35fa8cb263
-k <slowdive@me.com>
2018-06-05 23:17:01 -0400

    add offline fallback

--- sw.js
+++ sw.js
@@ -23,6 +23,8 @@ self.addEventListener('fetch', function(event) {
         cache.put(event.request, response.clone());
         return response;
       });
+    }).catch(function() {
+      return caches.match('/offline.html')
     })
   );
 });
