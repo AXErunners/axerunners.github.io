558402127387a69944a5062baed82994dbf6519c
-k <slowdive@me.com>
2018-06-06 16:34:55 -0400

    cache assets

--- sw.js
+++ sw.js
@@ -33,11 +33,47 @@ self.addEventListener('fetch', function(event) {
 var urlsToCache = [];
 
 // Cache assets
-{% for asset in site.static_files %}
-    {% if asset.path contains '/assets/images' or asset.path contains '/assets/posts' or asset.extname == '.js' %}
-    urlsToCache.push("{{ file.path }}")
-    {% endif %}
-{% endfor %}
+var filesToCache = [
+  '.',
+  '/assets/css/font-awesome.min.css',
+  '/assets/css/ie9.css',
+  '/assets/css/main.css',
+  '/assets/css/noscript.css',
+  '/assets/fonts/fontawesome-webfont.eot',
+  '/assets/fonts/fontawesome-webfont.svg',
+  '/assets/fonts/fontawesome-webfont.ttf',
+  '/assets/fonts/fontawesome-webfont.woff',
+  '/assets/fonts/fontawesome-webfont.woff2',
+  '/assets/fonts/FontAwesome.otf',
+  '/assets/images/axecore-ascii-screenshot.png',
+  '/assets/images/axerunners-wht.png',
+  '/assets/images/barterdex-btc-axe.png',
+  '/assets/images/bg.jpg',
+  '/assets/images/cntrls.jpg',
+  '/assets/images/dgma.jpg',
+  '/assets/images/minerdedication.jpg',
+  '/assets/images/overlay.png',
+  '/assets/images/splsh192.png',
+  '/assets/images/splsh512.png',
+  '/assets/images/tmbr.jpg',
+  '/assets/images/whtslft.jpg',
+  '/assets/js/jquery.min.js',
+  '/assets/js/main.js',
+  '/assets/js/skel.min.js',
+  '/assets/js/util.js'
+];
+
+var staticCacheName = 'axerunners-cache-v7';
+
+self.addEventListener('install', function(event) {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
 
 // Cache pages
 {% for page in site.html_pages %}
