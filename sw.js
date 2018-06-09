0d2cdd85e57030ce5891851e969801b91f7d8361
-k <slowdive@me.com>
2018-06-09 14:26:04 -0400

    add service worker

---
+++ sw.js
@@ -0,0 +1,38 @@
+---
+layout: null
+---
+var urlsToCache = [];
+
+var CACHE_NAME = 'axerunners-cache-v13';
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+var urlsToCache = [];
+
+// Cache pages
+{% for page in site.html_pages %}
+  urlsToCache.push("{{ page.url }}")
+{% endfor %}
