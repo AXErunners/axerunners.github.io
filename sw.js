888cf1636b735b65bfa261a15445f5c37f67354e
-k <slowdive@me.com>
2020-03-15 20:45:14 -0700

    Bump cache

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ layout: null
 ---
 var urlsToCache = [];
 
-var CACHE_NAME = 'axerunners-cache-v17';
+var CACHE_NAME = 'axerunners-cache-v18';
 
 self.addEventListener('install', function(event) {
   // Perform install steps
